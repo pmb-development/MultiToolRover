a0e1b5b54a7200fe6bd57d7fdf92075327eb79ec
Emil Berger <beremm14@htlkaindorf.at>
2019-04-29 14:54:01 +0200

    Add: SwiftSocket

--- MTR_Mobile/MTR_Mobile/ViewController.swift
+++ MTR_Mobile/MTR_Mobile/ViewController.swift
@@ -7,9 +7,42 @@
 //
 
 import UIKit
+import SwiftSocket
 
 class ViewController: UIViewController {
-
+    
+    var client: TCPClient = TCPClient(address: "", port: 1804)
+    var connected: Bool = false
+    
+    @IBOutlet weak var ipAddressTF: UITextField!
+    
+    @IBAction func connectTapped(_ sender: UIButton) {
+        client = TCPClient(address: ipAddressTF.text!, port: 1804)
+        connected = true
+    }
+    
+    @IBAction func disconnectTapped(_ sender: UIButton) {
+        connected = false
+    }
+    
+    @IBAction func velocityValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func speedModeSwitch(_ sender: Any) {
+    }
+    
+    @IBAction func moveForwardTapped(_ sender: Any) {
+    }
+    
+    @IBAction func moveBackwardTapped(_ sender: Any) {
+    }
+    
+    @IBAction func moveRightTapped(_ sender: Any) {
+    }
+    
+    @IBAction func moveLeftTapped(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
