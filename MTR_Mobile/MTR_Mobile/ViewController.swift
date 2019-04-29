088cac4b1a3edeb16a5ef927242333d38092258a
Emil Berger <beremm14@htlkaindorf.at>
2019-04-29 20:24:24 +0200

    Add: Data, ViewController

--- MTR_Mobile/MTR_Mobile/ViewController.swift
+++ MTR_Mobile/MTR_Mobile/ViewController.swift
@@ -11,17 +11,47 @@ import SwiftSocket
 
 class ViewController: UIViewController {
     
+    enum ConnectionError: Error {
+        case ipEmpty
+    }
+    
     var client: TCPClient = TCPClient(address: "", port: 1804)
     var connected: Bool = false
     
     @IBOutlet weak var ipAddressTF: UITextField!
     
     @IBAction func connectTapped(_ sender: UIButton) {
-        client = TCPClient(address: ipAddressTF.text!, port: 1804)
-        connected = true
+        do {
+            if (ipAddressTF.text!.isEmpty) {
+                throw ConnectionError.ipEmpty
+            }
+            client = TCPClient(address: ipAddressTF.text!, port: 1804)
+            let result = client.connect(timeout: 3)
+            if (result.isFailure) {
+                throw result.error!
+            }
+            connected = true
+        } catch (ConnectionError.ipEmpty) {
+            let alert = UIAlertController(title: "IP Address Error", message: "IP Address Field must no be empty!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        } catch (SocketError.connectionTimeout) {
+            let alert = UIAlertController(title: "Connection Error", message: "Connection timed out!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        } catch (SocketError.queryFailed) {
+            let alert = UIAlertController(title: "Connection Error", message: "Query failed!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        } catch (SocketError.connectionClosed) {
+            let alert = UIAlertController(title: "Connection Error", message: "Connection closed!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        } catch let unknownErrors {
+            print(unknownErrors)
+        }
     }
     
     @IBAction func disconnectTapped(_ sender: UIButton) {
+        client.close()
         connected = false
     }
     
